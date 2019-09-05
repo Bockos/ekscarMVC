45dfc8e5c4360eabfb668426345e58c64e68dacb
burak arslan <arslanburak91@gmail.com>
2019-09-05 16:00:41 +0300

    Custom css/js added bundles enableoptimizations disabled lyout cshtml configurated

--- ekscarMVC/App_Start/BundleConfig.cs
+++ ekscarMVC/App_Start/BundleConfig.cs
@@ -8,30 +8,31 @@ namespace ekscarMVC
         // Paketleme hakkında daha fazla bilgi için lütfen https://go.microsoft.com/fwlink/?LinkId=301862 adresini ziyaret edin
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/assets/js/jquery.min.js"));
+
+
+            // Geliştirme yapmak ve öğrenmek için Modernizr'ın geliştirme sürümünü kullanın. Daha sonra,
+            // üretim için hazır. https://modernizr.com adresinde derleme aracını kullanarak yalnızca ihtiyacınız olan testleri seçin.
+
+            bundles.Add(new ScriptBundle("~/Scripts/Base").Include(
+                        "~/assets/js/jquery.min.js",
+                        "~/assets/js/modernizr.js",
+                        "~/assets/js/script.js",
+                        "~/assets/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Template").Include(
+              "~/assets/js/wow.min.js",
+               "~/assets/js/slick.min.js",
+                "~/assets/js/parallax.js",
+                 "~/assets/js/select-chosen.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Custom").Include(
+                "~/assets/js/custom.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Geliştirme yapmak ve öğrenmek için Modernizr'ın geliştirme sürümünü kullanın. Daha sonra,
-            // üretim için hazır. https://modernizr.com adresinde derleme aracını kullanarak yalnızca ihtiyacınız olan testleri seçin.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/assets/js/modernizr.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/script").Include(
-                     "~/assets/js/script.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/assets/js/bootstrap.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/other").Include(
-                   "~/assets/js/wow.min.js",
-                    "~/assets/js/slick.min.js",
-                     "~/assets/js/parallax.js",
-                      "~/assets/js/select-chosen.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Styles/Template").Include(
           "~/assets/css/bootstrap-grid.css",
           "~/assets/css/icons.css",
            "~/assets/css/animate.min.css",
@@ -41,6 +42,11 @@ namespace ekscarMVC
                "~/assets/css/colors/colors.css",
                 "~/assets/css/bootstrap.css",
                  "~/Content/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/Styles/Custom").Include(
+             "~/assets/css/custom.css"));
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
